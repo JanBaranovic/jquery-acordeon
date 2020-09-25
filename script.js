655a8e7da231e739f6980e892eae71dc50694dc1
JanBaranovic <jan.baranovic77@gmail.com>
2020-09-25 13:34:27 +0200

    Loading ikonka a kopirovanie do zoznamu

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
+//Akordeon efekt
+
 const list = $('.jokes');
+const copy = $('.copy');
 
 list.find('dd').hide();
 
 list.find('dt').on('click', function(event) {
-	$(this).next().slideToggle()
+
+	const dt = $(this),
+		dd = dt.next();
+
+	dd.slideToggle()
 	.siblings('dd').slideUp();
+
+	const newContent = dd.text() + dd.html();
+
+	copy.append( newContent + '<hr>' )
 
 	event.preventDefault();
 })
+
+//Loading ikonka
+
+const element = $('.element');
+const loader = $('.loader');
+const body_width = $('body').width();
+
+element.on('click', function() {
+	loader.fadeIn();
+	$(this).animate({left: body_width - 250}, 2000, function() {
+		loader.fadeOut();
+	});
+});
+
+//Set timeout funkcia
+
+element.on('contextmenu', function(event) {
+	loader.fadeIn();
+	$(this).animate({left: body_width - 250}, 2000);
+
+	setTimeout(function() {
+		loader.fadeOut();
+	}, 4000);
+
+	event.preventDefault();
+});
+
+
+
